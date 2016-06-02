41c49dd6fc2c5b54da0a0256ca663349fef19674
Pankaj Patel <patel.pankaj@live.in>
2016-06-02 15:45:48 +0100

    randomized, but has anamolies; it can be much more better

--- src/js/data/game.js
+++ src/js/data/game.js
@@ -5,8 +5,13 @@ class Game {
     this.data = gameData;
     this.init();
   }
+
   init(){
     this.data.players.forEach( (player, i) => {
+      //random
+      this.randomizeWeapons(player);
+
+
       //weapons array
       if( player.weaponsIn == null ){
         let w = [];
@@ -18,6 +23,7 @@ class Game {
         })
         player.weaponsIn = w
       }
+
 
       //this loop filling is done to prevent mutation
       player.arena = [];
@@ -39,7 +45,45 @@ class Game {
       }
     })
   }
+  randomValue(arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  randomizeWeapons(player){
+    const direction = ['h', 'v'] //h: horizontal, v: vertical
+    if( typeof this.rows == 'undefined' ){ //DRY: dummy array creation for randoms
+      let rows = Array(this.data.arena.rows).fill(0)
+        rows = rows.map((e, i) => {
+          e = i;
+          return e;
+        } )
+      let cols = Array(this.data.arena.columns).fill(0)
+        cols = cols.map((e, i) => {
+          e = i;
+          return e;
+        } )
+      this.rows = rows;
+      this.cols = cols;
+    }
 
+    let rows = this.rows;
+    let cols = this.cols;
+
+    player.weapons.forEach( (weapon, index) => {
+      var dir = this.randomValue( direction );
+      var startX = this.randomValue( rows );
+      var startY = this.randomValue( cols );
+      // let length = this.data.weapons[weapon.type].squares;
+      weapon.cells = weapon.cells.map( i => {
+        if( dir == 'h' ){ //increase in c
+          return {r: startX, c: startY++}
+        } else { //increase in h
+          return {r: startX++, c: startY}
+        } //in future diagonal can also be done
+      })
+      // console.log(dir, {x: startX, y: startY}, weapon.type, length)
+      return player;
+    } )
+  }
   isWeaponized(player, r, c){
     let found = [];
     found = player.weaponsIn.filter( weapon => {
--- src/js/data/gameData.js
+++ src/js/data/gameData.js
@@ -3,57 +3,58 @@ var game = {
     rows: 10,
     columns: 10
   },
-  weapons: [
-    {
+  weapons: {
+    battleship: {
       name: 'Battleship',
       squares: 5,
       allowed: 1
-    }, {
+    },
+    destroyer: {
       name: 'Destroyer',
       squares: 4,
       allowed: 2
     }
-  ],
+  },
   players: [
     {
       id: 1,
       type: 'human',
       name: 'You',
-      arena: [],
+      arena: [], //will be filled by constructor
       weaponsIn: null,
       weapons: [
         {
-          name: 'Battleship',
+          type: 'battleship',
           cells: [{r: 0, c: 0},{r: 0, c: 1},{r: 0, c: 2},{r: 0, c: 3},{r: 0, c: 4}],
-          status: 1
+          status: 1 //active
         }, {
-          name: 'Destroyer',
+          type: 'destroyer',
           cells: [{r: 1, c: 0},{r: 1, c: 1},{r: 1, c: 2},{r: 1, c: 3}],
-          status: 1
+          status: 1 //active
         }, {
-          name: 'Destroyer',
+          type: 'destroyer',
           cells: [{r: 2, c: 0},{r: 2, c: 1},{r: 2, c: 2},{r: 2, c: 3}],
-          status: 1
+          status: 1 //active
         }
       ]
     }, {
       id: 2,
       type: 'bot',
       name: 'Computer',
-      arena: [],
+      arena: [], //will be filled by constructor
       weapons: [
         {
-          name: 'Battleship',
+          type: 'battleship',
           cells: [{r: 0, c: 0},{r: 0, c: 1},{r: 0, c: 2},{r: 0, c: 3},{r: 0, c: 4}],
-          status: 1
+          status: 1 //active
         }, {
-          name: 'Destroyer',
+          type: 'destroyer',
           cells: [{r: 1, c: 0},{r: 1, c: 1},{r: 1, c: 2},{r: 1, c: 3}],
-          status: 1
+          status: 1 //active
         }, {
-          name: 'Destroyer',
+          type: 'destroyer',
           cells: [{r: 2, c: 0},{r: 2, c: 1},{r: 2, c: 2},{r: 2, c: 3}],
-          status: 1
+          status: 1 //active
         }
       ]
 
